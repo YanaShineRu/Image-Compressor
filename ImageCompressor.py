bdd7d0569072c89525a7374f3893a56e283d80fa
Yana Shine <134070997+YanaShineRu@users.noreply.github.com>
2023-07-14 14:33:44 +0300

    Update ImageCompressor.py

--- ImageCompressor.py
+++ ImageCompressor.py
@@ -1,32 +1,9 @@
 import os
 import sys
+import cv2
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QFileDialog, QMessageBox, QProgressBar, QLabel, QSlider, QVBoxLayout, QHBoxLayout, QDesktopWidget
-from PyQt5.QtCore import Qt, QTimer, QObject, pyqtSignal
-from PIL import Image
+from PyQt5.QtCore import Qt
 from tqdm import tqdm
-
-class Worker(QObject):
-    finished = pyqtSignal()
-    progress_changed = pyqtSignal(int)
-    error_occurred = pyqtSignal(str)
-
-    def __init__(self, files, quality):
-        super().__init__()
-        self.files = files
-        self.quality = quality
-
-    def process_files(self):
-        for i, file_path in enumerate(self.files):
-            try:
-                image = Image.open(file_path)
-                image.save(file_path, optimize=True, quality=self.quality)
-            except Exception as e:
-                error_message = f"Ошибка при обработке файла: {file_path}\nОшибка: {str(e)}"
-                self.error_occurred.emit(error_message)
-            else:
-                self.progress_changed.emit(i + 1)
-
-        self.finished.emit()
 
 class ImageCompressor(QMainWindow):
     def __init__(self):
@@ -37,11 +14,9 @@ class ImageCompressor(QMainWindow):
 
         self.directory = None
         self.files = []
+        self.current_file = 0
         self.quality = 85
         self.error_count = 0
-        self.error_log = []
-
-        self.worker = None
 
         self.init_ui()
 
@@ -95,60 +70,64 @@ class ImageCompressor(QMainWindow):
     def compress_images(self):
         if self.directory:
             self.files = []
+            self.current_file = 0
             self.error_count = 0
-            self.error_log = []
 
-            for root, _, files in os.walk(self.directory):
-                for file in files:
-                    file_path = os.path.join(root, file)
-                    if file_path.endswith(('.jpg', '.jpeg', '.png', '.bmp')):
-                        self.files.append(file_path)
+            self.scan_directory(self.directory)
 
             if self.files:
                 self.progress_bar.setMaximum(len(self.files))
-                self.progress_label.setText(f'Обработано файлов 0/{len(self.files)}')
+                self.progress_label.setText(f'Обработано файлов {self.current_file + 1}/{len(self.files)}')
 
-                self.worker = Worker(self.files, self.quality)
-                self.worker.finished.connect(self.processing_completed)
-                self.worker.progress_changed.connect(self.update_progress)
-                self.worker.error_occurred.connect(self.handle_error)
-
-                QTimer.singleShot(0, self.worker.process_files)
+                self.process_files()
             else:
                 QMessageBox.warning(self, 'Ошибка', 'В выбранной директории нет изображений.')
         else:
             QMessageBox.warning(self, 'Ошибка', 'Директория не выбрана.')
 
-    def update_progress(self, value):
-        self.progress_bar.setValue(value)
-        self.progress_label.setText(f'Обработано файлов {value}/{len(self.files)}')
+    def scan_directory(self, directory):
+        for root, _, files in os.walk(directory):
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                self.files.append(file_path)
 
-    def handle_error(self, error_message):
-        print(error_message)
-        self.error_count += 1
-        self.error_log.append(error_message)
+    def process_files(self):
+        for file_path in tqdm(self.files[self.current_file:], desc='Обработка файлов', unit='файл'):
+            self.process_file(file_path)
+            self.current_file += 1
+            self.progress_bar.setValue(self.current_file)
+            self.progress_label.setText(f'Сжатие файла {self.current_file}/{len(self.files)}')
+
+        self.processing_completed()
+
+    def process_file(self, file_path):
+        try:
+            image = cv2.imread(file_path)
+            if image is not None:
+                cv2.imwrite(file_path, image, [cv2.IMWRITE_JPEG_QUALITY, self.quality])
+            else:
+                self.write_error_log(file_path, 'Ошибка чтения изображения')
+                self.error_count += 1
+        except Exception as e:
+            self.write_error_log(file_path, str(e))
+            self.error_count += 1
 
     def processing_completed(self):
-        QMessageBox.information(self, 'Готово', 'Сжатие изображений завершено.')
-
-        if self.error_count > 0:
-            self.create_error_log_file()
-
+        QMessageBox.information(self, f'Готово', f'Сжатие изображений завершено.\nОшибок: {self.error_count}')
         self.error_label.setText(f'<font color="red">Ошибки обработки: {self.error_count}</font>')
 
-    def create_error_log_file(self):
-        error_log_file = os.path.join(self.directory, 'Error.log')
-        with open(error_log_file, 'w') as f:
-            for error_message in self.error_log:
-                f.write(error_message)
-                f.write('\n')
-
     def closeEvent(self, event):
-        if self.worker is not None:
-            self.worker.finished.disconnect()
-            self.worker.progress_changed.disconnect()
-            self.worker.error_occurred.disconnect()
         QApplication.quit()
+
+    def write_error_log(self, file_path, error_message):
+        log_file_path = os.path.join(self.directory, 'error_log.txt')
+        try:
+            with open(log_file_path, 'a') as f:
+                f.write(f"Ошибка при обработке файла: {file_path}\n")
+                f.write(f"Ошибка: {error_message}\n")
+                f.write("------------------------\n")
+        except Exception as e:
+            print(f"Ошибка записи в журнал ошибок: {str(e)}")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
